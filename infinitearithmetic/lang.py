2b9c4d4a24e94bcea8c6d58801d9916ad9de9542
Justin <jbtcao@gmail.com>
2018-10-30 18:36:41 -0500

    Added `lex` to `lang` module
    We can't use lark because the testing server doesn't have pip. It's also in poor taste to use a library like that for this homework anyway, so we're writing our own lexer and parser. This commit adds the lexer.

--- infinitearithmetic/lang.py
+++ infinitearithmetic/lang.py
@@ -1,5 +1,11 @@
+import re
+from collections import namedtuple
+
 from . import bigint
 from lark import Lark, Transformer
+
+
+Token = namedtuple('Token', ['text', 'lineno', 'offset'])
 
 
 def eval_ast(ast):
@@ -22,6 +28,50 @@ class AstTransformer(Transformer):
 
 def eval_string(string):
     return eval_ast(string_to_ast(string))
+
+
+def lex(string):
+    return _do_lex(string, None, 1, 0, [])
+
+
+def _do_lex(string, filename, lineno, offset, acc):
+    if not string:
+        return acc
+    stripped = string.strip()
+
+    tok_offset = offset + (len(string) - len(stripped))
+
+    matches = [r.match(stripped) for r in _token_regexes]
+    matches = [m for m in matches if m]
+
+    if not matches:
+        raise SyntaxError('invalid syntax', (filename,
+                                             lineno, tok_offset, stripped))
+
+    if len(matches) > 1:
+        raise SyntaxError('ambiguous syntax',
+                          (filename, lineno, tok_offset, stripped))
+
+    match = matches[0]
+    new_str = match.re.sub('', stripped)
+    new_str_offset = tok_offset + (len(stripped) - len(new_str))
+    new_acc = acc + [Token(match.group(0), lineno, tok_offset)]
+
+    return _do_lex(new_str, filename, lineno, new_str_offset, new_acc)
+
+
+def _build_regex(pat):
+    return re.compile('^(%s)' % pat)
+
+
+_token_regexes = [
+    _build_regex('\\('),
+    _build_regex('\\)'),
+    _build_regex(','),
+    _build_regex('add'),
+    _build_regex('multiply'),
+    _build_regex('[\\d]+')
+]
 
 
 def string_to_ast(string):
--- tests/test_lang.py
+++ tests/test_lang.py
@@ -1,12 +1,16 @@
 from infinitearithmetic import bigint, lang
+from infinitearithmetic.lang import lex, Token
+
 
 def test__string_to_ast__parses_add():
     ast = lang.string_to_ast('add(5, 10)')
     assert lang.eval_ast(ast) == bigint.fromint(15, 1)
 
+
 def test__string_to_ast__parses_multiply():
     ast = lang.string_to_ast('multiply(5, 10)')
     assert lang.eval_ast(ast) == bigint.fromint(50, 1)
+
 
 def test__string_to_ast__parses_compound_calls():
     ast = lang.string_to_ast('multiply(add(5, 10), 10)')
@@ -14,3 +18,25 @@ def test__string_to_ast__parses_compound_calls():
 
     ast = lang.string_to_ast('multiply(5, add(multiply(2, 3), 10))')
     assert lang.eval_ast(ast) == bigint.fromint(80, 1)
+
+
+def test_lex():
+    # Recognizes standard tokens
+    assert(lex('add') == [Token('add', 1, 0)])
+    assert(lex('multiply') == [Token('multiply', 1, 0)])
+    assert(lex('()') == [Token('(', 1, 0), Token(')', 1, 1)])
+    assert(lex(',') == [Token(',', 1, 0)])
+
+    # Can lex complex expression
+    add_toks = [
+        Token('add', 1, 0),
+        Token('(', 1, 3),
+        Token('4', 1, 4),
+        Token(',', 1, 5),
+        Token('1', 1, 6),
+        Token(')', 1, 7)
+    ]
+    assert(lex('add(4,1)') == add_toks)
+
+    # Tracks whitespace
+    assert(lex('   (') == [Token('(', 1,  3)])
